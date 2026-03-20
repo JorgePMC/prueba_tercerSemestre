7a3b273a3bf7d9b31f03a493f95efe4b6296687d
JorgePMC <formalidad.jpg@gmail.com>
2026-03-20 11:27:38 -0300

    primer proyecto

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        System.out.println("A                         era el arc NO EL TARGET                               A");
+        System.out.println("A                      SOLO FUNCIONO CON MAVEN AAAAAAA                          A");
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    }
+}
